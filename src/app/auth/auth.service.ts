a0b30492da086770d3e51ef32bde3e9adf221fcf
anthony mero <anthony@gladys.io>
2020-06-24 16:54:38 +0200

    - Update auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,108 +1,94 @@
 import { Injectable } from '@angular/core';
-import { AuthOptions, WebAuth } from 'auth0-js';
+import * as auth0 from 'auth0-js';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
-@Injectable({
-  providedIn: 'root'
-})
+(window as any).global = window;
+
+@Injectable()
 export class AuthService {
+  // Create Auth0 web auth instance
+  auth0 = new auth0.WebAuth({
+    ClientID: environment.auth.clientID,
+    domain: environment.auth.domain,
+    responseType: 'token',
+    redirectUri: environment.auth.redirect,
+    audience: environment.auth.audience,
+    scope: environment.auth.scope
+  });
 
-  // tslint:disable-next-line:variable-name
-  protected _auth0Client: WebAuth;
-  private _accessToken: string;
-  private _idToken: string;
-  private _properties: AuthOptions;
+  // Store authentication data
+  expiresAt: number;
+  userProfile: any;
+  accessToken: string;
+  authenticated: boolean;
 
-  constructor() {
-
-    this._properties = {
-      clientID: 'zmDYcd0sUbs4FbDwF3EItwbHPwItqrsp',
-      domain: 'dev-oh3t1cwg.auth0.com',
-      responseType: 'token id_token',
-      audience: 'http://localhost:3000',
-      redirectUri: 'http://localhost:4200/login',
-      scope: 'openid profile'
-    };
-
-    this._auth0Client = new WebAuth({ ...this._properties });
+  constructor(
+    private readonly router: Router,
+  ) {
+    this.getAccessToken();
   }
 
-  public login(): void {
-    // trigger auth0 authentication page
-    this._auth0Client.authorize();
+
+  login(): void {
+    // Auth0 authorize request
+    this.auth0.authorize();
   }
 
-  public checkSession(): Promise<boolean> {
-    return new Promise<boolean>((resolve, reject) => {
-      this._auth0Client.checkSession(this._properties, async (error, authResult) => {
-        if (error && error.error !== 'login_required') {
-          // some other error
-          return reject(error);
-        } else if (error) {
-          // explicit authentication
-          this.handleAuthentication();
-          return resolve(false);
-        }
-        if (!this.isAuthenticated()) {
-          this._setSession(authResult);
-          return resolve(true);
-        }
-      });
-    });
-  }
-
-  public isAuthenticated(): boolean {
-    // Check whether the current time is past the
-    // Access Token's expiry time
-    return this._accessToken != null;
-  }
-
-  private handleAuthentication(): void {
-    this._auth0Client.parseHash((err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
+  handleLoginCallback() {
+    // When Auth0 hash parsed, get profile
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken) {
         window.location.hash = '';
-        this._setSession(authResult);
+        this.getUserInfo(authResult);
       } else if (err) {
-        console.log(err);
+        console.error(`Error: ${err.error}`);
+      }
+      this.router.navigate(['/']);
+    });
+  }
+
+  getAccessToken() {
+    this.auth0.checkSession({}, (err, authResult) => {
+      if (authResult && authResult.accessToken) {
+        this.getUserInfo(authResult);
       }
     });
   }
 
-  private _setSession(authResult): void {
-    this._accessToken = authResult.accessToken;
-    this._idToken = authResult.idToken;
-  }
-
-  // check if there is a property Admin in the access token
-  public isAdmin(): boolean {
-    if (this._accessToken) {
-      const helper = new JwtHelperService();
-      const decodedToken = helper.decodeToken(this._accessToken);
-      if (decodedToken['http://localhost:3000/roles'].indexOf('admin') > -1) {
-        return true;
-      } else {
-        return false;
+  getUserInfo(authResult) {
+    // Use access token to retrieve user's profile and set session
+    this.auth0.client.userInfo(authResult.accessToken, (err, profile) => {
+      if (profile) {
+        this._setSession(authResult, profile);
       }
-    } else {
-      return false;
-    }
+    });
   }
 
-  public getProfile(): object {
-    if (this._idToken) {
-      const helper = new JwtHelperService();
-      return helper.decodeToken(this._idToken);
-    }
+  private _setSession(authResult, profile) {
+    // Save authentication data and update login status subject
+    this.expiresAt = authResult.expiresIn * 1000 + Date.now();
+    this.accessToken = authResult.accessToken;
+    this.userProfile = profile;
+    this.authenticated = true;
   }
 
-  public getAccessToken(): string {
-    return this._accessToken;
+  logout() {
+    // Log out of Auth0 session
+    // Ensure that returnTo URL is specified in Auth0
+    // Application settings for Allowed Logout URLs
+    this.auth0.logout({
+      returnTo: 'http://localhost:4200',
+      clientID: environment.auth.clientID
+    });
   }
 
-  public logout(): void {
-    // Remove tokens
-    delete this._accessToken;
-    delete this._idToken;
+  get isLoggedIn(): boolean {
+    // Check if current date is before token
+    // expiration and user is signed in locally
+    return Date.now() < this.expiresAt && this.authenticated;
   }
+
 
 }
