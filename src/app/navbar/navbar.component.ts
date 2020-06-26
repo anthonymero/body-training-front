ba3b358c0fa8dbb99277e2eb67fbf5eb28d347ea
anthony mero <anthony@gladys.io>
2020-06-26 15:57:29 +0200

    - Create app material module - Create shared module - Declare app material module in shared module - Integrate navbar - Add custom css styles

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isConnected = true;
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router,
+  ) { }
 
   ngOnInit() {
+    // this.isConnected = this.isUserConnected();
+  }
+
+  onLogout(): void {
+    console.log('logout');
+    this.authService.logout();
+  }
+
+  onViewProfile(): void {
+    console.log('MyProfile');
+    // TODO set route /profile
+    this.router.navigate(['profile']);
+  }
+
+  private isUserConnected(): boolean {
+    return this.authService.isAuthenticated();
   }
 
 }
